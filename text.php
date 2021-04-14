ca65309013ce23eceef92d8c9cd89f550222ad06
Fran Díaz <fran.diaz.gonzalez@gmail.com>
2021-04-14 08:05:03 +0200

    Update text.php

--- text.php
+++ text.php
@@ -1,37 +1,22 @@
 <?php
-/**
- * Componente text
- */
 
 class text extends base_component implements components_interface {
 
 	public function make_text() : string {
-		global $_ITE, $_ITEC;
 		$html = '';
 
 		ob_start();
 		?>
-		<div class="<?=$this->cfg( 'contenido', 'alineado' )?>">
+		<div class="<?=$this -> cfg( 'contenido', 'alineado' )?>">
 			<?php
-			$text = $this->cfg('contenido','texto');
-			$variables = $_ITE -> funcs -> get_template_variables($text);
+			$text = $this -> cfg('contenido','texto');
+			$variables = $this -> _ITE -> funcs -> get_template_variables( $text );
 			if( ! empty( $variables ) ){ 
 				if( isset( $_REQUEST['d'] ) ) {
-					//$_ITEC_temp = $_ITEC;
-
 					$data = decode( $_REQUEST['d'] );
-
-					/*if( isset( $data['dsn']) ) {
-						$conn_data = $_ITEC -> get( 'system__connections', '*', ['dsn' => $data['dsn']] );
-						if( $conn_data ){
-							$_ITEC_temp = new ITE\db\db ( $conn_data, $_ITE );
-						}
-					}
-					
-					$d_info = $_ITEC_temp -> get( $data['table'], '*', [$data['table'].'_id' => $data['id']] );*/
-					$text = $_ITE -> funcs -> replace_in_template( $text, $data );
+					$text = $this -> _ITE -> funcs -> replace_in_template( $text, $data );
 				} else {
-					$text = $_ITE -> funcs -> replace_in_template( $text );
+					$text = $this -> _ITE -> funcs -> replace_in_template( $text );
 				}
 			}
 			echo $text;
@@ -46,6 +31,6 @@ class text extends base_component implements components_interface {
 	}
 
 	public function gen_content( ) : string {		
-		return $this -> make_text( $this -> component_info );
+		return $this -> make_text();
 	}
 }
